d741248f5bad97befb47ba66aa44ea36100fa299
dominpo <dominpo@hotmail.com>
2016-07-29 23:06:56 +0200

    modify publish

---
+++ ConnectingBots/Common/IAssistRequestService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IAssistRequestService
+    {
+        [OperationContract]
+        Task<int> CreateAssistRequest();
+
+        [OperationContract]
+        Task AddMessage(string msg);
+
+        [OperationContract]
+        Task<string> GetLastMessage();
+
+        [OperationContract]
+        Task<List<string>> GetAllMessages();
+
+
+    }
+}
