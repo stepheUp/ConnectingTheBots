80163cf4fed01be3d92b8fad1189a226fb0996e0
dominpo <dominpo@hotmail.com>
2016-07-30 18:10:13 +0200

    add again stateless assist service

--- ConnectingBots/AssistStatefulService/AssistStatefulService.cs
+++ ConnectingBots/AssistStatefulService/AssistStatefulService.cs
@@ -99,35 +99,19 @@ namespace AssistStatefulService
         /// This method executes when this replica of your service becomes primary and has write status.
         /// </summary>
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
- /*       protected override async Task RunAsync(CancellationToken cancellationToken)
+        protected override async Task RunAsync(CancellationToken cancellationToken)
         {
             // TODO: Replace the following sample code with your own logic 
             //       or remove this RunAsync override if it's not needed in your service.
 
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
-
-            while (true)
+              while (true)
             {
                 cancellationToken.ThrowIfCancellationRequested();
-
-                using (var tx = this.StateManager.CreateTransaction())
-                {
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
-
-                    ServiceEventSource.Current.ServiceMessage(this, "Current Counter Value: {0}",
-                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
-
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
-
-                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
-                    // discarded, and nothing is saved to the secondary replicas.
-                    await tx.CommitAsync();
-                }
 
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
             }
         }
-*/
+
         public async Task<int> CreateAssistRequest(string firstmessage)
         {
             var assistRequest = new AssistRequestItem();
