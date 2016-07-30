a163a9ed1602136f2fdbc067de89096d3475d3a0
Nicolas Delabarre <delabarre.nicolas@free.fr>
2016-07-30 03:39:53 +0200

    Initial delivery of KB Stateless service

---
+++ ConnectingBots/KBStatelessService/Models/QAItem.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBStatelessService.Models
+{
+    public class QAItem
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "question")]
+        public string Question { get; set; }
+
+        [JsonProperty(PropertyName = "response")]
+        public string Response { get; set; }
+    }
+}
