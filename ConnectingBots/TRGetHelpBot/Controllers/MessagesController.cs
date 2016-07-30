b825cacff2f9951f26355a42d04953dabcafeb4d
Benoit Le Pichon <belepich@microsoft.com>
2016-07-30 03:41:22 +0200

    LUIS integration

--- ConnectingBots/TRGetHelpBot/Controllers/MessagesController.cs
+++ ConnectingBots/TRGetHelpBot/Controllers/MessagesController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
 
 namespace TRGetHelpBot
 {
@@ -21,13 +22,7 @@ namespace TRGetHelpBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                await Conversation.SendAsync(activity, () => new GetHelpDialog());
             }
             else
             {
---
+++ ConnectingBots/TRGetHelpBot/GetHelpDialog.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TRGetHelpBot
+{
+    [LuisModel("253d1d82-9133-4915-bae5-f2c7952ab16a", "4a00615c199e49cfb8b13c4f72cdf965")]
+    [Serializable]
+    public class GetHelpDialog : LuisDialog<object>
+    {
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry I did not understand: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Questions")]
+        public async Task DeleteAlarm(IDialogContext context, LuisResult result)
+        {
+
+            await context.PostAsync("Question");
+
+            context.Wait(MessageReceived);
+        }
+        
+        [LuisIntent("Greating")]
+        public async Task FindAlarm(IDialogContext context, LuisResult result)
+        {
+
+            await context.PostAsync("Hello, how can I help you!");
+
+            context.Wait(MessageReceived);
+        }
+
+        public GetHelpDialog()
+        {
+        }
+
+        public GetHelpDialog(ILuisService service) : base(service)
+        {
+        }
+        
+    }
+}
